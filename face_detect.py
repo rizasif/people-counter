c8e2d916f1deb188983e2d6747a355b0d844c630
Rizwan Asif <rizasif@gmail.com>
2018-05-19 01:00:30 +0500

    face detect api

--- face_detect.py
+++ face_detect.py
@@ -1,29 +1,34 @@
-import numpy as np
-import cv2
-face_cascade = cv2.CascadeClassifier('model/haarcascade_profileface.xml')
+import face_recognition
+from PIL import Image
+
+image = face_recognition.load_image_file("data/test.png")
+faces = face_recognition.face_locations(image)
+
+print faces
+
+for location in faces:
+	top, right, bottom, left = location
+	print "Face Location {} {} {} {}".format(top,right,bottom,left)
+
+	face_image = image[top:bottom, left:right]
+	pil_image = Image.fromarray(face_image)
+	pil_image.show()
+
+# if len(faces) == 0:
+# 	print "No faces found"
  
-image = cv2.imread('data/test.png')
-grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# else:
+# 	print faces
+# 	print faces.shape
+# 	print "Number of faces detected: " + str(faces.shape[0])
  
-faces = face_cascade.detectMultiScale(grayImage)
+# 	for (x,y,w,h) in faces:
+# 		cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),1)
  
-print type(faces)
+# 	cv2.rectangle(image, ((0,image.shape[0] -25)),(270, image.shape[0]), (255,255,255), -1)
+# 	cv2.putText(image, "Number of faces detected: " + str(faces.shape[0]), (0,image.shape[0] -10), cv2.FONT_HERSHEY_TRIPLEX, 0.5,  (0,0,0), 1)
  
-if len(faces) == 0:
-	print "No faces found"
- 
-else:
-	print faces
-	print faces.shape
-	print "Number of faces detected: " + str(faces.shape[0])
- 
-	for (x,y,w,h) in faces:
-		cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),1)
- 
-	cv2.rectangle(image, ((0,image.shape[0] -25)),(270, image.shape[0]), (255,255,255), -1)
-	cv2.putText(image, "Number of faces detected: " + str(faces.shape[0]), (0,image.shape[0] -10), cv2.FONT_HERSHEY_TRIPLEX, 0.5,  (0,0,0), 1)
- 
-	cv2.imshow('Image with faces',image)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+# 	cv2.imshow('Image with faces',image)
+# 	cv2.waitKey(0)
+# 	cv2.destroyAllWindows()
 	
