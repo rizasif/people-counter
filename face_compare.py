13a8dfe497219884cfcf31fc03c47243619e8b5a
Rizwan Asif <rizasif@gmail.com>
2018-05-19 14:33:45 +0500

    3/9 2/57

--- face_compare.py
+++ face_compare.py
@@ -1,10 +1,17 @@
 import face_recognition
+import numpy as np
 
-image1 = face_recognition.load_image_file("data/test2.png")
-image2 = face_recognition.load_image_file("data/test2.png")
+image1 = face_recognition.load_image_file("data/faceC1.png")
+image2 = face_recognition.load_image_file("data/faceC2.png")
 
 face_encoding1 = face_recognition.face_encodings(image1)[0]
 face_encoding2 = face_recognition.face_encodings(image2)[0]
+
+face_encoding1 = np.array(face_encoding1)
+face_encoding2 = np.array(face_encoding2)
+
+print "Image 1: ", np.sqrt(np.sum(face_encoding1**2))
+print "Image 2: ", np.sqrt(np.sum(face_encoding2**2))
 
 results = face_recognition.compare_faces([face_encoding1], face_encoding2)
 print results
