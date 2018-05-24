d25dd7f6434da9482a6c30f2f211cb037cb222e4
Rizwan Asif <rizasif@gmail.com>
2018-05-24 15:40:26 +0500

    need to test on original video

---
+++ run.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+from shazam import Shazam
+
+SAMPLING_RATE = 4
+VIDEO_NAME = "test_sample.mp4"
+
+def process_file(filename, sampling_rate):
+	vidcap = cv2.VideoCapture(filename)
+	mShazam = Shazam()
+
+	success,image = vidcap.read()
+	skip = 0
+	while vidcap.isOpened():
+		if skip % sampling_rate == 0:
+			timestamp = vidcap.get(cv2.cv.CV_CAP_PROP_POS_MSEC)
+			
+			print "Processing Image at {}ms".format(timestamp)
+			mShazam.ProcessImage(image)
+		
+		skip += 1
+		success,image = vidcap.read()
+		if not success:
+			break
+	vidcap.release()
+	mShazam.printResults()
+	
+
+if __name__ == "__main__":
+	process_file("data/" + VIDEO_NAME, SAMPLING_RATE)
